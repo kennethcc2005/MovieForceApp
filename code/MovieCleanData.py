34a44d883b8ded73341ed3cc9abe2d6ba65546ff
Zoesh <zoesh@Zoeshs-MacBook-Pro.local>
2016-06-30 22:33:10 -0700

    update..

--- code/MovieCleanData.py
+++ code/MovieCleanData.py
@@ -11,102 +11,113 @@ import numpy as np
 import csv
 # import graphlab
 import os
+from collections import defaultdict
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.externals import joblib
 # class MovieCleanData(object):
 
 #Need to clean up the list before data preprocessing.
 def movie_keyword(keyword_file = '../data/keywords_clean.list', pickle_path = '../data/df_key'):    
-	df_key = pd.read_csv(keyword_file, delimiter='\n',header = None)
-	df_key['Movie'] = [df_key[0][i].split('\t')[0] for i in xrange(df_key.shape[0])]
-	df_key['Keyword'] = [df_key[0][i].split('\t')[-1] for i in xrange(df_key.shape[0])]
-	# df_key.to_pickle(pickle_path)
-	return df_key
+    df_key = pd.read_csv(keyword_file, delimiter='\n',header = None)
+    df_key['Movie'] = [df_key[0][i].split('\t')[0] for i in xrange(df_key.shape[0])]
+    df_key['Keyword'] = [df_key[0][i].split('\t')[-1] for i in xrange(df_key.shape[0])]
+    # df_key.to_pickle(pickle_path)
+    return df_key
 
 #If the dataframe has error, need to cleanup the list file manually.
 #Return a pivot dataframe with 27 movie genres. 
 def movie_genres(genres_file = '../data/genres.list',pickle_path = '../data/df_genres_pivot'):
-	df_genres = pd.read_csv(genres_file,delimiter= '\t', dtype = 'str', header = None)
-	df_genres['Genres'] = pd.concat([df_genres[6].dropna(),df_genres[5].dropna(),\
-							df_genres[4].dropna(),df_genres[3].dropna(),df_genres[2].dropna(),\
-							df_genres[1].dropna()]).reindex_like(df_genres)
-	df_genres.drop([1,2,3,4,5,6],axis = 1,inplace = True)
-	df_genres.columns = ['idx','Genres']
-	df_genres['cnt'] = 1
-	df_genres_pivot = df_genres.pivot_table(values = 'cnt', index = 'idx', columns = 'Genres')
-	# df_genres_pivot.to_pickle(pickle_path)
-	return df_genres_pivot
+    df_genres = pd.read_csv(genres_file,delimiter= '\t', dtype = 'str', header = None)
+    df_genres['Genres'] = pd.concat([df_genres[6].dropna(),df_genres[5].dropna(),\
+                            df_genres[4].dropna(),df_genres[3].dropna(),df_genres[2].dropna(),\
+                            df_genres[1].dropna()]).reindex_like(df_genres)
+    df_genres.drop([1,2,3,4,5,6],axis = 1,inplace = True)
+    df_genres.columns = ['idx','Genres']
+    df_genres['cnt'] = 1
+    df_genres_pivot = df_genres.pivot_table(values = 'cnt', index = 'idx', columns = 'Genres')
+    # df_genres_pivot.to_pickle(pickle_path)
+    return df_genres_pivot
 
 #The list file includes votes and rates for each movie; need to clean up the data manually.
 #Return DafaFrame with vote number greater 10000.
 def movie_rating(rating_file = '../data/ratings.list', min_vote_cnt = 10000):
-	df_rating = pd.read_csv(rating_file, delimiter='\t', dtype='unicode')
-	df_rating['Votes'] =  [int([i for i in df_rating.loc[j][0].split(' ') if i != ''][1]) \
-							for j in xrange(df_rating.shape[0])]
-	df_rating['Movie'] = [df_rating['New  Distribution  Votes  Rank  Title'][i][32:] \
-							for i in xrange(len(df_rating))]
+    df_rating = pd.read_csv(rating_file, delimiter='\t', dtype='unicode')
+    df_rating['Votes'] =  [int([i for i in df_rating.loc[j][0].split(' ') if i != ''][1]) \
+                            for j in xrange(df_rating.shape[0])]
+    df_rating['Movie'] = [df_rating['New  Distribution  Votes  Rank  Title'][i][32:] \
+                            for i in xrange(len(df_rating))]
 
-	#Exclude the tv dramas in dataframe.
-	df_tv = df_rating[df_rating.Movie.str.startswith('"')]
-	df_movie_rating = df_rating.drop(df_tv.index)
-	df_popular_movie = df_movie_rating[df_movie_rating.Votes > min_vote_cnt]
-	l = df_popular_movie.Movie
-	df_popular_movie.Movie = [i if i[0] != ' ' else i[1:] for i in l]
-	df_popular_movie = df_popular_movie.set_index(df_popular_movie.Movie)
-	# df_popular_movie.to_pickle('../data/df_popular_movie_10000')
-	return df_popular_movie
+    #Exclude the tv dramas in dataframe.
+    df_tv = df_rating[df_rating.Movie.str.startswith('"')]
+    df_movie_rating = df_rating.drop(df_tv.index)
+    df_popular_movie = df_movie_rating[df_movie_rating.Votes > min_vote_cnt]
+    l = df_popular_movie.Movie
+    df_popular_movie.Movie = [i if i[0] != ' ' else i[1:] for i in l]
+    df_popular_movie = df_popular_movie.set_index(df_popular_movie.Movie)
+    # df_popular_movie.to_pickle('../data/df_popular_movie_10000')
+    return df_popular_movie
 
 def movie_genres_rating(df_movie_genres, df_movie_rating): 
-	df_gen_vote = df_movie_rating.join(df_movie_genres)
-	df_gen_vote.drop(['New  Distribution  Votes  Rank  Title','Movie','_ Drama<>Mystery<>crime_'],axis = 1,inplace=True)
-	# df_gen_vote.to_pickle('../data/df_gen_vote')
-	return df_gen_vote
-	
+    df_gen_vote = df_movie_rating.join(df_movie_genres)
+    df_gen_vote.drop(['New  Distribution  Votes  Rank  Title','Movie','_ Drama<>Mystery<>crime_'],axis = 1,inplace=True)
+    # df_gen_vote.to_pickle('../data/df_gen_vote')
+    return df_gen_vote
+    
 def movie_force_data(df_key, df_gen_vote, number_keywords = 100):
-	df_clean_long = df_key.set_index(df_key.Movie).ix[df_gen_vote.index]
-	df = df_clean_long.ix[df_gen_vote.index[0]][:number_keywords]
-	for movie in df_gen_vote.index[1:]:
-		df = pd.concat([df,df_clean_long.ix[movie][:number_keywords]],axis =0)
-	df['count'] = 1
-	df100 = df[['Movie','Keyword','count']]
-	df100 = df100.pivot_table(values='count', index='Movie', columns='Keyword')
-	df_100key_genres = df100.join(df_gen_vote)
-	df_100key_genres.pop('Votes')
-	df_100key_genres = df_100key_genres.fillna(0)
-	# df_100key_genres.to_pickle('../data/df_100key_genres')
-	return df_100key_genres
-	
+    df_clean_long = df_key.set_index(df_key.Movie).ix[df_gen_vote.index]
+    df = df_clean_long.ix[df_gen_vote.index[0]][:number_keywords]
+    for movie in df_gen_vote.index[1:]:
+        df = pd.concat([df,df_clean_long.ix[movie][:number_keywords]],axis =0)
+    df['count'] = 1
+    df100 = df[['Movie','Keyword','count']]
+    df100 = df100.pivot_table(values='count', index='Movie', columns='Keyword')
+    df_100key_genres = df100.join(df_gen_vote)
+    df_100key_genres.pop('Votes')
+    df_100key_genres = df_100key_genres.fillna(0)
+    # df_100key_genres.to_pickle('../data/df_100key_genres')
+    return df_100key_genres
+
+def movie_dict(df_100key_genres, json_path = '../data/movie_dict.json'):
+    movie_dict = defaultdict(list)
+    for movie in df_100key_genres.index.values:
+        for keyword,value in df_100key_genres.loc[movie].iteritems():
+            if value == 1:
+                movie_dict[movie].append(keyword)
+    with open(json_path, 'w') as f:
+        json.dump(movie_dict, f)
+
 def movie_model(df_100key_genres, max_depth = 20, pickle_path = '../data/my_movie_model20_v10.pkl'):
-	X = df_100key_genres.values
-	y = df_100key_genres.index
-	clf = DecisionTreeClassifier('entropy',max_depth = max_depth)
-	clf.fit(X,y)
-	# clf.score(X,y)
-	# joblib.dump(clf, pickle_path)
-	return clf
+    X = df_100key_genres.values
+    y = df_100key_genres.index
+    clf = DecisionTreeClassifier('entropy',max_depth = max_depth)
+    clf.fit(X,y)
+    # clf.score(X,y)
+    # joblib.dump(clf, pickle_path)
+    return clf
 
 def save_feature_movie_name(df_100key_genres, save_path = '../data/feature_movie_name'):
-	movie_list = df_100key_genres.index
-	feature_list = df_100key_genres.columns.values
-	np.savez(save_path,movie_list,feature_list)
+    movie_list = df_100key_genres.index
+    feature_list = df_100key_genres.columns.values
+    np.savez(save_path,movie_list,feature_list)
 
 def save_model_features(clf,save_path = '../data/my_movie_model20_10_array'):
-	np.savez(save_path,clf.tree_.children_left,clf.tree_.children_right,clf.tree_.feature,clf.tree_.threshold)
+    np.savez(save_path,clf.tree_.children_left,clf.tree_.children_right,clf.tree_.feature,clf.tree_.threshold)
 
 def save_model_value(clf, save_path = '../data/my_movie_model20_10_value'):
-	valuefiles = clf.tree_.value
-	sparse_values = sparse.lil_matrix(valuefiles.astype(int))
-	np.save(save_path,sparse_values)
+    valuefiles = clf.tree_.value
+    sparse_values = sparse.lil_matrix(valuefiles.astype(int))
+    np.save(save_path,sparse_values)
 
 if __name__ == '__main__':
-	df_popular_movie = movie_rating(rating_file = '../data/ratings.list', min_vote_cnt = 10000)
-	df_genres_pivot = movie_genres(genres_file = '../data/genres.list',pickle_path = '../data/df_genres_pivot')
-	df_gen_vote = movie_genres_rating(df_movie_genres = df_genres_pivot, df_movie_rating = df_popular_movie)
-	df_key = movie_keyword(keyword_file = '../data/keywords_clean.list', pickle_path = '../data/df_key')
-	df_100key_genres = movie_force_data(df_key = df_key, df_gen_vote = df_gen_vote, number_keywords = 100)
-	clf = movie_model(df_100key_genres = df_100key_genres, max_depth = 20, pickle_path = '../data/my_movie_model20_v10.pkl')
-	save_feature_movie_name(df_100key_genres = df_100key_genres)
-	save_model_features(clf = clf)
-	save_model_value(clf = clf)
+    df_popular_movie = movie_rating(rating_file = '../data/ratings.list', min_vote_cnt = 10000)
+    df_genres_pivot = movie_genres(genres_file = '../data/genres.list',pickle_path = '../data/df_genres_pivot')
+    df_gen_vote = movie_genres_rating(df_movie_genres = df_genres_pivot, df_movie_rating = df_popular_movie)
+    df_key = movie_keyword(keyword_file = '../data/keywords_clean.list', pickle_path = '../data/df_key')
+    df_100key_genres = movie_force_data(df_key = df_key, df_gen_vote = df_gen_vote, number_keywords = 100)
+    movie_dict(df_100key_genres, json_path = '../data/movie_dict.json')
+    clf = movie_model(df_100key_genres = df_100key_genres, max_depth = 20, pickle_path = '../data/my_movie_model20_v10.pkl')
+    save_feature_movie_name(df_100key_genres = df_100key_genres)
+    save_model_features(clf = clf)
+    save_model_value(clf = clf)
 
 
--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -5,8 +5,8 @@ import socket
 import time
 from datetime import datetime
 import numpy as np 
-# # from fuzzywuzzy import fuzz
-# # from fuzzywuzzy import process
+# from fuzzywuzzy import fuzz
+# from fuzzywuzzy import process
 from MoviePosters import MoviePosters
 import urllib
 import scipy
@@ -14,10 +14,10 @@ import scipy
 app = Flask(__name__)
 
 # clf = joblib.load('my_movie_model20_v10.pkl')
-moviefiles = np.load('/home/ubuntu/flaskapp/data/feature_movie_name.npz')
-modelfiles = np.load('/home/ubuntu/flaskapp/data/my_movie_model20_10_array.npz')
-valuefiles = np.load('/home/ubuntu/flaskapp/data/my_movie_model20_10_value.npy')
-
+moviefiles = np.load('data/feature_movie_name.npz')
+modelfiles = np.load('data/my_movie_model20_10_array.npz')
+valuefiles = np.load('data/my_movie_model20_10_value.npy')
+movie_dict = json.load('data/movie_dict.json')
 feature_list = moviefiles['arr_1']
 movie_list = moviefiles['arr_0']
 
@@ -54,6 +54,45 @@ def next_feature_threshold_left_right_child(current_idx, tree_left = tree_childr
     threshold = threshold[current_idx]
     return feature_idx, threshold, left_idx, right_idx
 
+def next_guess_model(movie_features, user_answers, movie_dict):
+    for i in xrange(len(movie_features)):
+                true_list = []
+                false_list = []
+                temp = user_answers
+                if temp[i] == 'TRUE': 
+                    temp[i] ='FALSE'
+                else: temp[i] = 'TRUE'
+                for k, v in enumerate(movie_features):
+                    if temp[k] == 'TRUE':
+                        true_list.append(v)
+                    else:
+                        false_list.append(v)
+                for movie, features in movie_dict.iteritems():
+                    if (set(true_list).issubset(set(features)) and (not set(false_list).issubset(set(features))):
+                        next_movie_list.append(movie)
+                newdict=defaultdict(list)
+                for i in next_movie_list:
+                    newdict[i] = movie_dict[i]
+                df = pd.DataFrame(columns = ['name','features','count'])
+                ix = 0
+                for k, v in newdict.items():
+                    for i in v:
+                        df.loc[ix] = [k,i,1]
+                        ix+=1
+                df_new = df.pivot_table(values='count', index='name', columns='features', aggfunc='mean')
+                df_new.fillna(0,inplace = True)
+                clf = DecisionTreeClassifier('entropy',max_depth = 20)
+                clf.fit(df_new.values,np.array(df_new.index))
+                movie_list = df_new.index
+                feature_list = df_new.columns.values
+                X = feature_list
+                tree_children_left = clf.tree_.children_left
+                tree_children_right = clf.tree_.children_right
+                tree_feature = clf.tree_.feature
+                tree_threshold = clf.tree_.threshold
+                tree_value = clf.tree_.value
+                idx = 0
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -70,6 +109,7 @@ def movie_guess(X = feature_list, column_names = movie_list, idx = 0):
     user_questions = []
     user_answers = []
     user_keyword = []
+    movie_features = []
     feature_idx, threshold, left_idx, right_idx = next_feature_threshold_left_right_child(idx)
 
     if request.method == 'GET':
@@ -83,6 +123,11 @@ def movie_guess(X = feature_list, column_names = movie_list, idx = 0):
         return_answer = request.get_json()['answer']
         last_left_idx = request.get_json()['left_idx']
         last_right_idx = request.get_json()['right_idx']
+        next_round = request.get_json()['wrong_guess']
+        next_movie_list = []
+        if next_round == 'TRUE':
+            next_guess_model(movie_features, user_answers, movie_dict)
+
         if return_answer == 'TRUE':
             idx = last_right_idx
             user_answers.append('TRUE')
@@ -111,7 +156,7 @@ def movie_guess(X = feature_list, column_names = movie_list, idx = 0):
             question = question_type(X[feature_idx])
         user_questions.append(question)
         user_keyword.append(feature_idx)
-
+        movie_features.append(X[feature_idx])
         return jsonify({ 'question': question, 'left_idx': updated_left_idx, 'right_idx': updated_right_idx})        
 
 @app.route('/movie_features', methods=['GET', 'POST'])
